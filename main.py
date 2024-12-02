5ffc61a08615e3ea6c2c0f15cba4d84e7320f1bf
Rohithshan <rohithshan23@gmail.com>
2024-12-02 14:20:44 +0530

    transformations

---
+++ main.py
@@ -0,0 +1,28 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import when, col
+
+spark = SparkSession.builder.appName("Pyspark_example").getOrCreate()
+
+df = spark.read.csv('telecom_details.csv', header=True, inferSchema=True)
+
+error_table = df.filter(df['Contract Start Date'].isNull() | df['Contract End Date'].isNull())\
+
+valid_table = df.subtract(error_table).withColumn(
+    "Product Name",
+    when(df['Product Name'].isNull(), "ItNULL").otherwise(df['Product Name'])
+).withColumn(
+    "Product ID",
+    when(col('Product Name') == 'Phone', '3d541dcd')
+    .when(col('Product Name') == 'SmartWatch', 'bb28fb32')
+    .when(col('Product Name') == 'Tablet', '01dc1d3a')
+    .otherwise('000AA')
+)
+
+error_table.coalesce(1).write.mode('overwrite').csv('error_table', header=True)
+valid_table.coalesce(1).write.mode('overwrite').csv('valid_table', header=True)
+
+print(error_table.count())
+print(valid_table.count())
+
+print("Error table success")
+spark.stop()
